783bc5979d06a5d7924bb8a0da33964b152c701b
se040054 <peter8822967@gmail.com>
2023-07-21 01:45:09 +0800

    router init

--- app.js
+++ app.js
@@ -6,6 +6,37 @@ app.get('/',(req,res)=>{
   res.send('hello world')
 })
 
+app.get('/todos',(req,res)=>{
+  res.send('get all todos')
+})
+
+app.get('/todos/new',(req,res)=>{
+  res.send('get create todo page')
+})
+
+app.post('/todos',(req,res)=>{
+  res.send('add todo')
+})
+
+
+app.get('/todos/:id',(req,res)=>{
+  res.send(`get todo : ${req.params.id}`)
+})
+
+app.get('/todos/:id/edit',(req,res)=>{
+  res.send(`edit todo : ${req.params.id}`)
+})
+
+app.put('/todos/:id',(req,res)=>{
+  res.send(`modify todo `)
+})
+
+app.delete('/todos/:id',(req,res)=>{
+  res.send(`delete todo`)
+})
+
+
+
 app.listen(3000,()=>{
   console.log('Click : http://localhost:3000/')
 })
