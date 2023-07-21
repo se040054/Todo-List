0e0cfa014de857e58a270470e89eabed9a862b46
se040054 <peter8822967@gmail.com>
2023-07-22 02:40:34 +0800

    install & initialize sequelize

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express=require('express')
 const app=express()
 
+const db=require('./models')
+const Todo=db.Todo
 
 app.get('/',(req,res)=>{
   res.send('hello world')
 })
 
 app.get('/todos',(req,res)=>{
-  res.send('get all todos')
+  return Todo.findAll()
+            .then((todos)=> res.send({todos}))
+            .catch((err)=>{res.status(422).json(err)})
 })
 
 app.get('/todos/new',(req,res)=>{
