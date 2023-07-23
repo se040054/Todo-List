01d7ddcc84c7c21a40c2b9fbec1af30de47d5de0
se040054 <peter8822967@gmail.com>
2023-07-23 09:46:58 +0800

    implement deleting todo

--- app.js
+++ app.js
@@ -75,7 +75,11 @@ app.put('/todos/:id',(req,res)=>{
 })
 
 app.delete('/todos/:id',(req,res)=>{
-  res.send(`delete todo`)
+  const id=req.params.id
+  return Todo.destroy({
+    where:{id}
+  }).then(()=>{res.redirect('/todos')})
+  
 })
 
 
